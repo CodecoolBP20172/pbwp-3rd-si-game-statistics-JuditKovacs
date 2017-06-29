253cb954b8eaf1ef66c6f6d4fe5c4952bed32cd0
Jones <medonica1105@gmail.com>
2017-06-29 19:46:15 +0200

    clean

--- reports.py
+++ reports.py
@@ -7,57 +7,55 @@ def count_games(file_name):
     game_list.close()
 
 
-def decide(file_name, year):
+def get_stat_list(file_name):
     file_list = []
-    nested_list = 0
-    year_list = []
     game_list = open(file_name, 'r')
     for line in game_list.readlines():
         fields = line.split('\t')
         file_list.append(fields)
-    for i in range(len(file_list)):
-            year_list.append(file_list[nested_list][2])
-            nested_list += 1
-            year_list = [int(i) for i in year_list]
-            if year in year_list:
-                return True
+    return file_list
     game_list.close()
+
+def years(file_name):
+    line_list = get_stat_list(file_name)
+    year_list = []
+    nested_list = 0
+    for i in range(len(line_list)):
+            year_list.append(line_list[nested_list][2])
+            nested_list += 1
+    return year_list
+
+
+
+def decide(file_name, year):
+    list_of_years = years(file_name)
+    list_of_years = [int(i) for i in list_of_years]
+    if year in list_of_years:
+        return True
+
 
 
 def get_latest(file_name):
-    file_list = []
-    year_list = []
-    x = 0
-    v = 0
-    game_list = open(file_name, 'r')
-    for line in game_list.readlines():
-        fields = line.split('\t')
-        file_list.append(fields)
-    for i in range(len(file_list)):
-            year_list.append(file_list[x][2])
-            x += 1
-    for item in range(len(file_list)):
-        if max(year_list) in file_list[v]:
-            return(file_list[v][0])
+    list_of_years = years(file_name)
+    line_list = get_stat_list(file_name)
+    nested_list = 0
+    for item in range(len(line_list)):
+        if max(list_of_years) in line_list[nested_list]:
+            return(line_list[nested_list][0])
             break
-        v += 1
-    game_list.close()
+        nested_list += 1
+
 
 
 def count_by_genre(file_name, genre):
-    file_list = []
+    line_list = get_stat_list(file_name)
     nested_list = 0
     number = 0
-    game_list = open(file_name, 'r')
-    for line in game_list.readlines():
-        fields = line.split('\t')
-        file_list.append(fields)
-    for nested_list in range(len(file_list)):
-        if genre in file_list[nested_list]:
+    for nested_list in range(len(line_list)):
+        if genre in line_list[nested_list]:
             number += 1
     nested_list += 1
     return(number)
-    game_list.close()
 
 
 def get_line_number_by_title(file_name, title):
@@ -74,3 +72,4 @@ def get_line_number_by_title(file_name, title):
                 raise ValueError
         except ValueError as err:
             return "There in no game with this title"
+    game_list.close()
